f57d9ecfc15dd9c5f56289eb2371d8bf5412a016
Dmitry Sergeev <dmitry.sergeev@booking.com>
2017-11-05 20:43:48 +0100

    re-route helpers to local resources

--- default/role.builder.experimental.js
+++ default/role.builder.experimental.js
@@ -15,7 +15,7 @@ module.exports = {
 	    if(creep.memory.building) {
             const route = Game.map.findRoute(creep.room, 'W53N46');
             if(route.length > 0) {
-                creep.say('Now heading to room '+route[0].room);
+                creep.say('To '+route[0].room);
                 const exit = creep.pos.findClosestByRange(route[0].exit);
                 creep.moveTo(exit);
             }else {
@@ -30,37 +30,58 @@ module.exports = {
 
             }
 	    }
-	    else {
+        else {
             const route = Game.map.findRoute(creep.room, 'W53N44');
             if(route.length > 0) {
-                creep.say('Now heading to room '+route[0].room);
-                const exit = creep.pos.findClosestByRange(route[0].exit);
-                creep.moveTo(exit);
-            }else {
-                let target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-                if (target) {
-                    if (creep.pickup(target) === ERR_NOT_IN_RANGE) {
+                var target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
+                if(target) {
+                    if(creep.pickup(target) === ERR_NOT_IN_RANGE) {
                         creep.moveTo(target);
                     }
                 } else {
-                    let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+
+                    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                         filter: s => ( (  s.structureType === STRUCTURE_CONTAINER ||
                             s.structureType === STRUCTURE_STORAGE) &&
-                            s.store[RESOURCE_ENERGY] > (creep.carryCapacity - creep.carry.energy) / 2) ||
-                            s.structureType === STRUCTURE_LINK && s.energy > (creep.carryCapacity - creep.carry.energy) / 2
+                            s.store[RESOURCE_ENERGY] > (creep.carryCapacity - creep.carry.energy) * 2) ||
+                            s.structureType === STRUCTURE_LINK && s.energy > (creep.carryCapacity - creep.carry.energy) * 2
                     });
-                    if (container) {
-                        if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                            creep.moveTo(container);
-                        }
-                    } else {
-                        let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-                        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-                            creep.moveTo(source);
-                        }
+                }
+
+                if (target) {
+                    if (creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                } else {
+                    target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                    if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                }
+
+                if (!target){
+                    creep.say('To '+route[0].room);
+                    const exit = creep.pos.findClosestByRange(route[0].exit);
+                    creep.moveTo(exit);
+                }
+            } else {
+                let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: s => (  (s.structureType === STRUCTURE_CONTAINER ||
+                        s.structureType === STRUCTURE_STORAGE) &&
+                        s.store[RESOURCE_ENERGY] > (creep.carryCapacity - creep.carry.energy) / 2) ||
+                        s.structureType === STRUCTURE_LINK && s.energy > (creep.carryCapacity - creep.carry.energy) / 2
+                });
+                if (container) {
+                    if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(container);
+                    }
+                } else {
+                    let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                    if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source);
                     }
                 }
             }
-	    }
-	}
+        }
+    }
 };
--- default/role.upgrader.experimental.js
+++ default/role.upgrader.experimental.js
@@ -27,10 +27,38 @@ module.exports =  {
         else {
             const route = Game.map.findRoute(creep.room, 'W53N44');
             if(route.length > 0) {
-                creep.say('Now heading to room '+route[0].room);
-                const exit = creep.pos.findClosestByRange(route[0].exit);
-                creep.moveTo(exit);
-            }else {
+                var target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
+                if(target) {
+                    if(creep.pickup(target) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                } else {
+
+                    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                        filter: s => ( (  s.structureType === STRUCTURE_CONTAINER ||
+                            s.structureType === STRUCTURE_STORAGE) &&
+                            s.store[RESOURCE_ENERGY] > (creep.carryCapacity - creep.carry.energy) * 2) ||
+                            s.structureType === STRUCTURE_LINK && s.energy > (creep.carryCapacity - creep.carry.energy) * 2
+                    });
+                }
+
+                if (target) {
+                    if (creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                } else {
+                    target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                    if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                }
+
+                if (!target){
+                    creep.say('To '+route[0].room);
+                    const exit = creep.pos.findClosestByRange(route[0].exit);
+                    creep.moveTo(exit);
+                }
+            } else {
                 let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: s => (  (s.structureType === STRUCTURE_CONTAINER ||
                         s.structureType === STRUCTURE_STORAGE) &&
