f9310fd93898dc0e8bb1c28fa8fe2791e20dcfe5
Dmitry Sergeev <dmitry.sergeev@booking.com>
2017-11-11 19:59:02 +0100

    cleanup main

--- sim/main.js
+++ sim/main.js
@@ -4,15 +4,11 @@ require('prototype.Room');
 require('prototype.Creep');
 require('prototype.RoomObject');
 require('prototype.Source');
-let creepRoles = require('creep.roles');
-let roleSpawn = require('role.spawn');
-let roleLink = require('role.link');
 let roleRoom = require('role.room');
 require('prototype.StructureTower');
 
 module.exports.loop = function () {
     try {
-
         for (let name in Memory.creeps) {
             if (!Game.creeps[name]) {
                 console.log('Deleting: '+ name);
@@ -26,25 +22,11 @@ module.exports.loop = function () {
                 console.log('Clearing non-existing creep memory:', name);
             }
         }
-
         let rooms = Game.rooms;
-
         for (let name in rooms){
             let room = rooms[name];
             roleRoom.run(room);
         }
-
-        let spawns = Game.spawns;
-        for(let name in spawns){
-            let spawn = spawns[name];
-            roleSpawn.run(spawn);
-        }
-
-        for (let name in Game.creeps) {
-            let creep = Game.creeps[name];
-            creepRoles.run(creep);
-        }
-
     } catch (e) {
         console.log('Brain Exeception', e);
     }
