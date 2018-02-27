fd5b429ae1b0f2476384d3f7d32882a31d171ba8
Dmitry Sergeev <dmitry.sergeev@booking.com>
2018-02-27 19:55:52 +0100

    Quick add tower

---
+++ default/prototype.tower.js
@@ -0,0 +1,22 @@
+StructureTower.prototype.defend =
+    function () {
+        let target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (target) {
+            this.attack(target);
+        } else {
+            let target = this.pos.findClosestByRange(FIND_CREEPS, {
+                filter: (s) => s.hits < s.hitsMax
+            });
+            if (target) {
+                this.heal(target);
+            } else {
+                let structure = this.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (s) => (s.hits < s.hitsMax / 1.5 && s.structureType !== STRUCTURE_WALL && s.structureType !== STRUCTURE_RAMPART) ||
+                        ((s.structureType === STRUCTURE_RAMPART || s.structureType === STRUCTURE_WALL) && s.hits < 1000)
+                });
+                if (structure) {
+                    this.repair(structure);
+                }
+            }
+        }
+    };
